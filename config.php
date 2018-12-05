a8ce1e96277c11a9376affc210dc0cdfea7bdbcd
fikrifar <40161137+fikrifar@users.noreply.github.com>
2018-12-05 13:28:22 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$config = [
+"imei" => "868473037901071",
+"sign" => "6eb55e8eba64bc71250d75a1f17141045a65d2f3",
+"token" => "5b4fbefeced1c58e3e608490ac4d4380",
+"uuid" => "cba9b745-0ad2-44fd-929b-cf8e5422c4ba"
+];
